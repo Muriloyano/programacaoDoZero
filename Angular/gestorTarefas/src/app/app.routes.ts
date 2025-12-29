d3d9d4bc47ec9420d7bcfc75377d6c3bf0766f41
Murilo Yano <muril0yano150107@gmail.com>
2025-12-29 16:56:08 -0300

    criando html dos componentes

--- Angular/gestorTarefas/src/app/app.routes.ts
+++ Angular/gestorTarefas/src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { TaskDetails } from './task-details/task-details';
 
 export const routes: Routes = [
     {path: 'taskList', title: 'Lista de Tarefas', component: TaskList},
-    {path: 'taskDetail', title: 'Detalhes da Tarefa', component: TaskDetails},
+    {path: 'taskDetails', title: 'Detalhes da Tarefa', component: TaskDetails},
     {path: '', redirectTo: 'taskList', pathMatch: 'full'},
 ];
