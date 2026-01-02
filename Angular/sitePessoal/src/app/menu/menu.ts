e93e0fe2806cd8184943b556d77dab15d1ae0399
Murilo Yano <muril0yano150107@gmail.com>
2026-01-02 18:40:57 -0300

    barra menu

--- Angular/sitePessoal/src/app/menu/menu.ts
+++ Angular/sitePessoal/src/app/menu/menu.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { MenuService } from '../services/menu';
+import { Menu } from '../models/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {
+export class MenuComponent {
 
   constructor(private menuService: MenuService) { }
+
+  menuStatus  = 'closed';
+
+  menuItens?: Array<Menu>; 
+
+  ngOnInit() {
+    const menu = this.menuService.getItem();
+
+    this.menuItens = menu;
+  }
+
+  menuClick() {
+    this.menuStatus = (this.menuStatus === 'open' ? 'closed' : 'open');
+
+    
+  }
 }
